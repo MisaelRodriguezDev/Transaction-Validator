c71358be580c2f4ffa96ba5e48cd1fb95a828b52
Misael_Rodríguez <misaelrdz963@gmail.com>
2025-12-10 18:16:19 -0600

    fix: deploy .sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Convierte el nombre del repositorio a minúsculas
+REPO_LOWER=$(echo "$GITHUB_REPOSITORY_OWNER" | tr '[:upper:]' '[:lower:]')
+
 echo "Construyendo imagen Docker..."
-docker build -t ghcr.io/$GITHUB_REPOSITORY_OWNER/transaction-validator:latest .
-docker push ghcr.io/$GITHUB_REPOSITORY_OWNER/transaction-validator:latest
+docker build -t ghcr.io/$REPO_LOWER/transaction-validator:latest .
+docker push ghcr.io/$REPO_LOWER/transaction-validator:latest
